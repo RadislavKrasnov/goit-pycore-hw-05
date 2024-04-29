bfb3576e876e0c87f1aa8ebce81e3a0154a1c0e7
Radyslav Krasnov <radislavkrasnoff@gmail.com>
2024-04-29 09:28:17 +0300

    rename task four and three folders

--- task_three/main.py
+++ task_three/main.py
@@ -1,36 +1,18 @@
-"""Helper bot module"""
+"""Application entry point"""
 
-from input_parser import parse_input
-from command_handler import (
-    add_contact,
-    change_contact,
-    show_phone,
-    show_all,
-)
+import sys
+from log_reader import load_logs
+from log_counter import count_logs_by_level
+from log_output import display_log_counts, display_log_details_by_level
 
-def main():
-    """Helper bot logic"""
-    contacts = {}
-    print("Welcome to the assistant bot!")
-    while True:
-        user_input = input("Enter a command: ")
-        command, *args = parse_input(user_input)
-
-        if command in ["close", "exit"]:
-            print("Good bye!")
-            break
-        elif command == "hello":
-            print("How can I help you?")
-        elif command == "add":
-            print(add_contact(args, contacts))
-        elif command == "change":
-            print(change_contact(args, contacts))
-        elif command == "phone":
-            print(show_phone(args, contacts))
-        elif command == "all":
-            show_all(contacts)
-        else:
-            print("Invalid command.")
+def main() -> None:
+    file_path = sys.argv[1]
+    error_level = sys.argv[2]
+    logs = load_logs(file_path)
+    counts = count_logs_by_level(logs)
+    display_log_counts(counts)
+    print()
+    display_log_details_by_level(logs, error_level)
 
 if __name__ == "__main__":
     main()
